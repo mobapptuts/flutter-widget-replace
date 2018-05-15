5d5b2a842350fbbc3ad613dcf3164510502f9458
Nigel Henshaw <nigel@mobapptuts.com>
2018-05-15 09:59:38 +0900

    Implemented image downloado

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const String _imageUrlOne = 'https://static.pexels.com/photos/596940/pexels-photo-596940.jpeg';
+
 void main() => runApp(new MaterialApp(title: 'Wiget Switcher', home: new LoadImage(),));
 
 class LoadImage extends StatefulWidget {
@@ -16,6 +18,23 @@ class LoadImageState extends State<LoadImage> {
       appBar: new AppBar(
         title: new Text('Image Downloaded'),
       ),
+      body: new ImageUrl(
+        imageUrl: _imageUrlOne,
+      ),
+    );
+  }
+}
+
+class ImageUrl extends StatelessWidget {
+  final String imageUrl;
+  ImageUrl({this.imageUrl});
+  @override
+  Widget build(BuildContext context) {
+    return new Image.network(
+      imageUrl,
+      fit: BoxFit.cover,
+      width: double.infinity,
+      height: double.infinity,
     );
   }
 
