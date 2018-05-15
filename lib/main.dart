cc1e3cbee320f43d88eceb7323a9fa0e387eba44
Nigel Henshaw <nigel@mobapptuts.com>
2018-05-15 10:57:49 +0900

    Added photos list

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 
 const String _imageUrlOne = 'https://static.pexels.com/photos/596940/pexels-photo-596940.jpeg';
+const String _imageUrlTwo = 'https://images.pexels.com/photos/163065/mobile-phone-android-apps-phone-163065.jpeg';
+const String _imageUrlThree = 'https://static.pexels.com/photos/47261/pexels-photo-47261.jpeg';
 
 void main() => runApp(new MaterialApp(title: 'Wiget Switcher', home: new LoadImage(),));
+
+class Photo {
+  final Text title;
+  final String url;
+  const Photo({this.title, this.url});
+}
+
+const List<Photo> photos = <Photo> [
+  const Photo(title: const Text('ImageOne'), url: _imageUrlOne),
+  const Photo(title: const Text('ImageTwo'), url: _imageUrlTwo),
+  const Photo(title: const Text('ImageThree'), url: _imageUrlThree),
+];
 
 class LoadImage extends StatefulWidget {
   @override
